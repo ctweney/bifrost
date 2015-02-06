c3a5bd6047705b4c3edef8b425f924a1bb9b186a
Chris Tweney <chris@media.berkeley.edu>
2015-02-05 16:07:37 -0800

    Remove unused param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  rescue_from CanCan::AccessDenied do |exception|
+  rescue_from CanCan::AccessDenied do
     redirect_to '/denied'
   end
 
