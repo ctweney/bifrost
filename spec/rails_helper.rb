ffd9ec18c715a986089a3358c1b196850281e1c7
Chris Tweney <chris@media.berkeley.edu>
2015-02-17 16:45:42 -0800

    Make rspec output more documentary

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,8 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
 
+  config.formatter = :documentation
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
