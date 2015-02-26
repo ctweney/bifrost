94aae10e1444b9a96aac8089cc282c2cf6635fc8
Steve Chan <sychan@berkeley.edu>
2015-02-26 11:40:31 -0800

    Added sychan as an admin in the seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,5 @@ Role.create({role_id: 2, name: 'Editor', description: 'Can manage all objects ex
 Role.create({role_id: 3, name: 'Viewer', description: 'Can view all objects'})
 
 Person.create(calnet_uid: '904715', name: 'Chris Tweney').roles << admin
+Person.create(calnet_uid: '1013580', name: 'Steve Chan').roles << admin
 
