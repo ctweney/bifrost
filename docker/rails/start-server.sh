6a1a8575a55eeb337a56129acffb4cc4e5543f46
Chris Tweney <chris@media.berkeley.edu>
2015-02-26 11:03:43 -0800

    Setup database when needed; compile assets on startup

--- docker/rails/start-server.sh
+++ docker/rails/start-server.sh
@@ -6,6 +6,13 @@ cd /app
 cp -n config/secrets.yml.example config/secrets.yml
 bundle install
 
+# init the db only if needed
+echo "Checking to see if database needs to be created..."
+bundle exec rake database:create_if_necessary
+
+echo "Compiling assets..."
+bundle exec rake assets:precompile
+
 echo "Migrating database..."
 bundle exec rake db:migrate
 
