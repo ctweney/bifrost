b9ff8793091662ff3e93e6277522769ef367c140
Chris Tweney <chris@media.berkeley.edu>
2015-02-27 11:06:37 -0800

    Add team members to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,8 @@ Role.create({role_id: 2, name: 'Editor', description: 'Can manage all objects ex
 Role.create({role_id: 3, name: 'Viewer', description: 'Can view all objects'})
 
 Person.create(calnet_uid: '904715', name: 'Chris Tweney').roles << admin
-
+Person.create(calnet_uid: '10746', name: 'Russ Connacher').roles << admin
+Person.create(calnet_uid: '1013580', name: 'Steve Chan').roles << admin
+Person.create(calnet_uid: '980853', name: 'Tamer Sakr').roles << admin
+Person.create(calnet_uid: '304844', name: 'Adam Cohen').roles << admin
+Person.create(calnet_uid: '17232', name: 'Karin Bliman').roles << admin
