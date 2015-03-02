0565f210d61a59d3876023e55256691257d648e6
Chris Tweney <chris@media.berkeley.edu>
2015-03-02 14:00:49 -0800

    SISRP-1827 Implement an IP whitelist feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  before_filter :check_ip
+
+  after_filter :log_access
+
   rescue_from CanCan::AccessDenied do
     redirect_to '/denied'
   end
@@ -26,6 +30,46 @@ class ApplicationController < ActionController::Base
     else
       redirect_to person_omniauth_authorize_path :cas
     end
+  end
+
+  def check_ip
+    valid = false
+    whitelist.each do |addr|
+      if addr.matches? remote
+        valid = true
+        break
+      end
+    end
+    unless valid
+      Rails.logger.warn "Received a request from remote IP that is not on IP whitelist: #{remote}"
+      render :nothing => true, :status => 403
+    end
+  end
+
+  def whitelist
+    Rails.cache.fetch('ip_whitelist', expires_in: 24.hours) do
+      whitelist = []
+      Rails.application.secrets['ip_whitelist'].each do |addr|
+        whitelist << NetAddr::CIDR.create(addr)
+      end
+      whitelist
+    end
+  end
+
+  def log_access
+    line = "ACCESS_LOG #{remote} #{request.request_method} #{request.filtered_path} #{status}"
+    if current_person.present?
+      line += " uid=#{current_person.calnet_uid}"
+    else
+      line += " uid=NONE"
+    end
+    line += " class=#{self.class.name} action=#{params["action"]} view=#{view_runtime}ms db=#{db_runtime}ms"
+    Rails.logger.warn line
+  end
+
+  def remote
+    # HTTP_X_FORWARDED_FOR is the client's IP when we're behind Apache; REMOTE_ADDR otherwise
+    request.env['HTTP_X_FORWARDED_FOR'] || request.env['REMOTE_ADDR']
   end
 
 end
